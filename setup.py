d6440a90d3c4d471cee5276d16ca133917504f6a
shreyas <141219160+shreyas-lyzr@users.noreply.github.com>
2024-02-09 00:02:41 +0530

    Update increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lyzr-automata",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "openai==1.3.4",
