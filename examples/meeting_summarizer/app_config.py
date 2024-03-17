fc9eb57c6155e1fc0dc02a8be3a7ef525ca55fb6
rasswanth-lyzr <rasswanth@lyzr.ai>
2024-03-17 11:04:18 +0530

    fix: config file

--- examples/meeting_summarizer/app_config.py
+++ examples/meeting_summarizer/app_config.py
@@ -2,8 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 # Application (client) ID of app registration
 CLIENT_ID = os.getenv("CLIENT_ID")
+
 # Application's generated client secret: never check this into source control!
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
@@ -19,15 +21,10 @@ REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirec
 # The absolute URL must match the redirect URI you set
 # in the app's registration in the Azure portal.
 
-# You can find more Microsoft Graph API endpoints from Graph Explorer
-# https://developer.microsoft.com/en-us/graph/graph-explorer
 ENDPOINT = (
     "https://graph.microsoft.com/v1.0/users"  # This resource requires no admin consent
 )
 
-# You can find the proper permission names from this document
-# https://docs.microsoft.com/en-us/graph/permissions-reference
-# SCOPE = ["User.ReadBasic.All", "OnlineMeetings.ReadWrite"]
 SCOPE = [
     "User.ReadBasic.All",
     "OnlineMeetings.ReadWrite",
@@ -37,5 +34,3 @@ SCOPE = [
 
 # Tells the Flask-session extension to store sessions in the filesystem
 SESSION_TYPE = "filesystem"
-# Using the file system will not work in most production systems,
-# it's better to use a database-backed session store instead.
