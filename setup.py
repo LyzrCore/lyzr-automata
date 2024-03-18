9f762d47a6ca28501a35f67855b2b020825e395e
shreyas <141219160+shreyas-lyzr@users.noreply.github.com>
2024-03-18 20:31:33 +0530

    Update version to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lyzr-automata",
-    version="0.1.1",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "openai==1.3.4",
