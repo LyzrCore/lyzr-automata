d071dea15ab5d3772d51d1e59d8035b13365fd08
rasswanth-lyzr <rasswanth@lyzr.ai>
2024-03-26 17:39:25 +0530

    update: add comments

--- examples/storyboard_generator/main.py
+++ examples/storyboard_generator/main.py
@@ -5,7 +5,10 @@ input_scene = st.text_area("Enter Scene Description")
 
 button=st.button('Submit')
 if (button==True):
+    # CALL FUNCTION TO GENERATE OUTPUT
     generated_output = storyboard_generator(input_scene)
+
+    # DISPLAY OUTPUT
     text_output = generated_output[0]['task_output']
     st.write(text_output)
     image_file_name = generated_output[1]['task_output'].local_file_path
